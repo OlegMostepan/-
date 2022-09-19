a2ed6502e51f01ede42cd5a019f35b171e088630
OMP <95340045+OlegMostepan@users.noreply.github.com>
2022-09-19 15:31:43 +0300

    Add files via upload

---
+++ Task 66 Program.cs
@@ -0,0 +1,26 @@
+﻿
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int Sum (int M, int N)
+{
+    if (M < N) 
+    {
+    return M + Sum(++M,N);
+    }
+    return M;   
+}
+
+void PrintArray (int sum)
+{   
+    Console.Write($"Сумма чисел = {sum}"); 
+} 
+
+Console.WriteLine("Введите любое положительное число ");
+int M= int.Parse(Console.ReadLine());
+Console.WriteLine("Введите любое положительное число ");
+int N= int.Parse(Console.ReadLine());
+int sum = Sum(M,N);
+PrintArray(sum);
---
+++ Task 68 Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+// m = 2, n = 3 -> Akkerman(m,n) = 9
+
+ int Akkerman(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        if(m > 0 && n == 0)
+        {
+            return Akkerman(m - 1, 1);
+        }
+        if(m > 0 && n > 0)
+        {
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+        }
+        return m;
+    }
+
+    
+void PrintArray (int result, int M, int N)
+{   
+    Console.Write($"Akkerman({M},{N}) = {result}"); 
+} 
+
+ Console.WriteLine("Введите любое положительное число ");
+int M= int.Parse(Console.ReadLine());
+Console.WriteLine("Введите любое положительное число ");
+int N= int.Parse(Console.ReadLine());
+int result = Akkerman(M,N);
+PrintArray(result, M, N);
+  
